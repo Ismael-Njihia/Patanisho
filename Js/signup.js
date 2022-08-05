f0193b83ebcb6451849f7e43f54eee0f133ff03f
EngIshmael <engIshmaelnjihia@gmail.com>
2022-08-05 23:37:52 +0300

    signup authentication

---
+++ Js/signup.js
@@ -0,0 +1,32 @@
+document.getElementById("signUp").onclick = function(){
+    var Fname= document.getElementById("inputFName").value;
+    var Sname= document.getElementById("inputSName").value;
+    var phoneNo = document.getElementById("phoneNumber").value;
+    var email = document.getElementById("inputEmail").value;
+    var password = document.getElementById("inputPassword").value;
+    var cPassword= document.getElementById("confirmPassword").value;
+
+   if(password == cPassword){
+   //sign in the user
+   firebase.auth().createUserWithEmailAndPassword(email, password).then((userCred)=>{
+    const userId = userCred.user.uid;
+
+    firebase.firestore().collection("users").doc(userId).set({
+        Fname: Fname,
+        Sname: Sname,
+        phoneNo: phoneNo,
+        email: email,
+        userId: userId,
+
+    }).then(()=>{
+        alert("User Created")
+    }).catch((error)=>{
+        var error1 = message.error;
+        alert(error1);
+    })
+   })
+
+   }else{
+    alert("Password not matching!")
+   }
+}
---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJIg9nlppw-JXTM6C_ehLzRx3Uu7gO6zw",
+    authDomain: "patanisho-fc484.firebaseapp.com",
+    projectId: "patanisho-fc484",
+    storageBucket: "patanisho-fc484.appspot.com",
+    messagingSenderId: "707889017484",
+    appId: "1:707889017484:web:0a570eee7e845537b501a9"
+  };
+  firebase.initializeApp(firebaseConfig);
